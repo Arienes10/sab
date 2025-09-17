037da0b10359a2930c5ffd97156124e37a09f78c
Arienes10 <56785786+Arienes10@users.noreply.github.com>
2025-09-17 22:48:35 +0800

    Update sab.lua

--- sab.lua
+++ sab.lua
@@ -1,28 +1,27 @@
 script_key="BYvfnaCXOEdoyNWGmkkjcOZkMMLiaOYY";
-setfpscap(12)
+setfpscap(5)
 
 getgenv().sabConfig = {
     -- General
-    KICK_PLAYER_LOBBY_AMOUNT = 7,  -- kick from game if 3+ players in lobby
+    KICK_PLAYER_LOBBY_AMOUNT = 99,  -- kick from game if 99+ players in lobby
     CHASE_BRAINROT = false,  -- true -> chase | false -> stand at end of conveyor
     MAX_CASH = math.huge,  -- can add number 999999, math.huge = always collect
-    COLLECT_CASH_COOLDOWN = 120,
-    LOCK_GATE = false,
-    PRIVATE_SERVER = true,
+    COLLECT_CASH_COOLDOWN = 180,
+    LOCK_GATE = true,
 
     -- Brainrot
     PRIORITY_BRAINROT = { "Secret Lucky Block", "Admin Lucky Block", "Taco Lucky Block" },  -- Keep brainrot & priority buy
     OPEN_LUCKY_BLOCK = { "Mythic Lucky Block" },
 
     -- Tools
-    AUTO_BUY_TOOL = { "Speed Coil" },
-    EQUIP_TOOL = { "Speed Coil" },  -- Equip left to right
+    AUTO_BUY_TOOL = { "Gummy Slap", "Splatter Slap", "Glitch Slap", "Galaxy Slap", "Nuclear Slap", "Flame Slap", "Dark Matter Slap", "Ruby Slap", "Emerald Slap", "Diamond Slap", "Gold Slap", "Iron Slap", "Slap", "Speed Coil" },
+    EQUIP_TOOL = { "Gummy Slap", "Splatter Slap", "Glitch Slap", "Galaxy Slap", "Nuclear Slap", "Flame Slap", "Dark Matter Slap", "Ruby Slap", "Emerald Slap", "Diamond Slap", "Gold Slap", "Iron Slap", "Slap", "Speed Coil" },  -- Equip left to right
 
     -- Rebirth
-    MAX_REBIRTH = 15,
+    MAX_REBIRTH = 6,
     KICK_AFTER_REBIRTH = false,
     BRAINROT_RARITY_STOP_REBIRTH = { "Secret", "Admin", "OG" },
-    IGNORE_BRAINROT_GENERATION_STOP_REBIRTH = 10000000,  -- Stop Rebirth if have 300k+/s brainrot 
+    IGNORE_BRAINROT_GENERATION_STOP_REBIRTH = 300000,  -- Stop Rebirth if have 300k+/s brainrot 
 
     -- Webhook
     BRAINROT_WEBHOOK_URL = "https://discord.com/api/webhooks/1408456072309772440/30JDnShUvXtSGPTMF1LmrR80fLk_dWQku2QnSSglThJ4chAHjUP7CIVDjCnMnqtfJWrU",
