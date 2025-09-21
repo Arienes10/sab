fa25ff55e6118db0c462053ebfe5a60278bf2f28
Arienes10 <56785786+Arienes10@users.noreply.github.com>
2025-09-21 11:27:55 +0800

    Update sab.lua

--- sab.lua
+++ sab.lua
@@ -22,7 +22,7 @@ getgenv().sabConfig = {
     -- Rebirth
     MAX_REBIRTH = 10,
     KICK_AFTER_REBIRTH = false,
-    BRAINROT_RARITY_STOP_REBIRTH = { "Secret", "Admin", "OG" },
+    --BRAINROT_RARITY_STOP_REBIRTH = { "Secret", "Admin", "OG" },
     IGNORE_BRAINROT_GENERATION_STOP_REBIRTH = 10000000,  -- Stop Rebirth if have 300k+/s brainrot 
 
     -- Webhook
