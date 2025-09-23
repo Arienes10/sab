0ed238e1353a9ff2c78b5514939b33cce21093ea
Arienes10 <56785786+Arienes10@users.noreply.github.com>
2025-09-23 18:51:13 +0800

    Update sab.lua

--- sab.lua
+++ sab.lua
@@ -3,7 +3,7 @@ setfpscap(15)
 
 getgenv().sabConfig = {
     -- General
-    CLONE_PRIVATE_SERVER = true,
+    CLONE_PRIVATE_SERVER = false,
     KICK_PLAYER_LOBBY_AMOUNT = 5,  -- kick from game if 3+ players in lobby
     CHASE_BRAINROT = false,  -- true -> chase | false -> stand at end of conveyor
     MAX_CASH = math.huge,  -- can add number 999999, math.huge = always collect
