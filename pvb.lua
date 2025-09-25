1dd23e6bda5d3e2712146fa1f3db614324310ee1
Arienes10 <56785786+Arienes10@users.noreply.github.com>
2025-09-25 13:06:46 +0800

    Update pvb.lua

--- pvb.lua
+++ pvb.lua
@@ -17,8 +17,8 @@ getgenv().pvbConfig = {
     SELL_PLANT_DELAY = 20,
 
     -- Webhook
-    BRAINROT_WEBHOOK_URL = "",
-    DISCORD_ID = "",
+    BRAINROT_WEBHOOK_URL = "https://discord.com/api/webhooks/1408456072309772440/30JDnShUvXtSGPTMF1LmrR80fLk_dWQku2QnSSglThJ4chAHjUP7CIVDjCnMnqtfJWrU",
+    DISCORD_ID = "622997465607700510",
     NOTIFY_RARITY = { "Secret", "Limited" },
     NOTIFY_NAME = {},
     NOTIFY_WEIGHT = math.huge,
