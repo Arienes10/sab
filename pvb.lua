e23fd2bcb2e033381e2ba16a7cc800289faf3ebb
Arienes10 <56785786+Arienes10@users.noreply.github.com>
2025-09-27 21:08:35 +0800

    Update pvb.lua

--- pvb.lua
+++ pvb.lua
@@ -4,13 +4,13 @@ getgenv().pvbConfig = {
     AUTO_UPDATE_RESTART = true,
     MAX_FPS = 3,  -- This will override setfpscap()
     LOW_CPU = true,
-    MAX_REBIRTH = 2,
+    MAX_REBIRTH = 99,
     FROST_GRENADE_TARGET_MAX_HP = 100000,  -- Use frost grenade 100k+ hp brainrot
     BRAINROT_RARITY_STOP_REBIRTH = {"Secret", "Limited"},
     OPEN_LUCKY_EGG = {"Godly Lucky Egg", "Secret Lucky Egg", "Meme Lucky Egg"},
     FUSE_PLANT = {"Mr Carrot"},
 
-    BUY_SEED_SHOP = {["Cactus"] = 5, ["Strawberry"] = 5, ["Pumpkin"] = 5, ["Sunflower"] = 5, ["Dragon Fruit"] = 5, ["Eggplant"] = 5, ["Watermelon"] = 5, "Cocotank", "Carnivorous Plant", "Mr Carrot", "Tomatrio"},
+    BUY_SEED_SHOP = {["Cactus"] = 5, ["Strawberry"] = 5, ["Pumpkin"] = 5, ["Sunflower"] = 5, ["Dragon Fruit"] = 5, ["Eggplant"] = 5, ["Watermelon"] = 5, "Cocotank", "Carnivorous Plant", "Mr Carrot", "Tomatrio", "Shroombino"},
     BUY_GEAR_SHOP = {"Frost Grenade", "Frost Blower"},
     KEEP_SEED = {},
     KEEP_PLANT_RARITY = {"Secret", "Limited"},
