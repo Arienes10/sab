21d8cd96d8b06d6e6f6905d6c27db03b035905e6
Arienes10 <56785786+Arienes10@users.noreply.github.com>
2025-09-27 20:47:21 +0800

    Update pvb.lua

--- pvb.lua
+++ pvb.lua
@@ -22,7 +22,7 @@ getgenv().pvbConfig = {
 
     -- Webhook
     BRAINROT_WEBHOOK_URL = "https://discord.com/api/webhooks/1408456072309772440/30JDnShUvXtSGPTMF1LmrR80fLk_dWQku2QnSSglThJ4chAHjUP7CIVDjCnMnqtfJWrU",
-    DISCORD_ID = "622997465607700510",
+    DISCORD_ID = "1387823456518934528",
     NOTIFY_RARITY = { "Secret", "Limited" },
     NOTIFY_NAME = { "Mr Carrot", "Tomatrio", "Tralalelo Tralala", "Bombini Gussini" },
     NOTIFY_WEIGHT = math.huge,
