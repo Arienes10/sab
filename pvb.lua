342a30f41dcb61e9ab578b7acf76b0897b3e2dcf
Arienes10 <56785786+Arienes10@users.noreply.github.com>
2025-10-04 22:35:57 +0800

    Update pvb.lua

--- pvb.lua
+++ pvb.lua
@@ -11,12 +11,7 @@ getgenv().pvbConfig = {
     OPEN_LUCKY_EGG = {"Godly Lucky Egg", "Secret Lucky Egg", "Meme Lucky Egg"},
     FUSE_PLANT = {"Mr Carrot"},
 
-    BUY_SEED_SHOP = {["Cactus"] = 5, ["Strawberry"] = 5, ["Pumpkin"] = 5, ["Sunflower"] = 5, ["Dragon Fruit"] = 5, ["Eggplant"] = 5, ["Watermelon"] = 5, "Cocotank", "Carnivorous Plant", "Mr Carrot", "Tomatrio", "Shroombino"},
-    BUY_GEAR_SHOP = {"Frost Grenade", "Frost Blower"},
-    KEEP_SEED = {},
-    KEEP_PLANT_RARITY = {"Secret", "Limited"},
-    KEEP_BRAINROT_MONEY_PER_SECOND = 10000,  -- Number
-    KEEP_BRAINROT_RARITY = {"Secret", "Limited"},
+    BUY_SEED_SHOP = {["Cactus"] = 5, ["Strawberry"] = 5, ["Pumpkin"] = 5, ["Sunflower"] = 5, ["Dragon Fruit"] = 5, ["Eggplant"] = 5, ["Watermelon"] = 5, "Cocotank", "Carnivorous Plant", "Mr Carrot", "Tomatrio", "Shroombino", "Mango"},
 
     SELL_BRAINROT_DELAY = 30,
     SELL_PLANT_DELAY = 30,
