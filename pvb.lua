24f66388c1870e02fa2e9592a0cac3620b338411
Arienes10 <56785786+Arienes10@users.noreply.github.com>
2025-10-04 22:36:23 +0800

    Update pvb.lua

--- pvb.lua
+++ pvb.lua
@@ -12,7 +12,14 @@ getgenv().pvbConfig = {
     FUSE_PLANT = {"Mr Carrot"},
 
     BUY_SEED_SHOP = {["Cactus"] = 5, ["Strawberry"] = 5, ["Pumpkin"] = 5, ["Sunflower"] = 5, ["Dragon Fruit"] = 5, ["Eggplant"] = 5, ["Watermelon"] = 5, "Cocotank", "Carnivorous Plant", "Mr Carrot", "Tomatrio", "Shroombino", "Mango"},
+    BUY_GEAR_SHOP = {"Frost Grenade", "Frost Blower"},
+    KEEP_SEED = {},
+    KEEP_PLANT_RARITY = {"Secret", "Limited"},
+    KEEP_BRAINROT_MONEY_PER_SECOND = 20000,  -- Number
+    KEEP_BRAINROT_RARITY = {},
 
+    SELL_BRAINROT_DELAY = 30,
+    SELL_PLANT_DELAY = 30,
     SELL_BRAINROT_DELAY = 30,
     SELL_PLANT_DELAY = 30,
 
